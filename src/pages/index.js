c6ecbe254f0db934c8c2ae059c758c00916cca71
Shujat Khalid <khalidshujat@gmail.com>
2020-04-08 16:41:44 +0100

    Add initial home page image resources

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import homePageImageLarge from "../images/home-page/home-page-large.png"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <img src={homePageImageLarge} width="100%"></img>
+    <button>Find Events</button>
+  </div>
+)
