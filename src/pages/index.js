2637095f459f0fc9e67b4dbfcabc6721273b82ba
Joseph Kempster <jkempster34@gmail.com>
2020-04-09 15:29:31 +0100

    Add responsive "inspire the next generation" image
    - Add sass and css modules to improve styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
-import React from "react"
-import homePageImageLarge from "../images/home-page/home-page-large.png"
+import React from "react";
+import indexStyles from "./index.module.scss";
+import inspireTheNextGeneration from "../images/home-page/inspire-the-next-generation.png";
+import inspireTheNextGenerationSmall from "../images/home-page/inspire-the-next-generation-small.png";
 
 export default () => (
-  <div>
-    <img src={homePageImageLarge} width="100%"></img>
-    <button>Find Events</button>
+  <div style={{ maxWidth: 1200, margin: "auto" }}>
+    <img
+      src={inspireTheNextGeneration}
+      alt="Inspire the next generation"
+      className={indexStyles.inspireImage}
+    />
+    <img
+      src={inspireTheNextGenerationSmall}
+      alt="Inspire the next generation"
+      className={indexStyles.inspireImageSmall}
+    />
+    <button className={indexStyles.findEventsButton}>Find Events</button>
   </div>
-)
+);
