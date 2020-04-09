72d94d99a58df6c34ccf21e848f9995d67babd49
Joseph Kempster <jkempster34@gmail.com>
2020-04-09 16:16:23 +0100

    Add navigation to life as a teacher page
     - Link to life as a teacher page on large image uses anchor sized with CSS instead of image maps. This is to allow the link to resize with the viewport.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react";
+import { Link } from "gatsby";
 import indexStyles from "./index.module.scss";
 import inspireTheNextGeneration from "../images/home-page/inspire-the-next-generation.png";
 import inspireTheNextGenerationSmall from "../images/home-page/inspire-the-next-generation-small.png";
 
 export default () => (
-  <div style={{ maxWidth: 1200, margin: "auto" }}>
-    <img
-      src={inspireTheNextGeneration}
-      alt="Inspire the next generation"
-      className={indexStyles.inspireImage}
-    />
-    <img
-      src={inspireTheNextGenerationSmall}
-      alt="Inspire the next generation"
-      className={indexStyles.inspireImageSmall}
-    />
+  <div className={indexStyles.container}>
+    <div id={indexStyles.inspireImageLargeContainer}>
+      <Link id={indexStyles.anchorLifeAsATeacher} to="/life-as-a-teacher/" />
+      <img
+        src={inspireTheNextGeneration}
+        alt="Inspire the next generation"
+        className={indexStyles.inspireImage}
+      />
+    </div>
+    <Link to="/life-as-a-teacher/">
+      <img
+        src={inspireTheNextGenerationSmall}
+        alt="Inspire the next generation"
+        className={indexStyles.inspireImageSmall}
+      />
+    </Link>
     <button className={indexStyles.findEventsButton}>Find Events</button>
   </div>
 );
