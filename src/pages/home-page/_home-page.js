e1b06797675484dd754d918f88a35235b40bd71b
Joseph Kempster <jkempster34@gmail.com>
2020-04-15 14:05:03 +0100

    Replace header image with styled h1

--- src/pages/home-page/_home-page.js
+++ src/pages/home-page/_home-page.js
@@ -4,11 +4,11 @@ import styles from "./home-page.module.scss";
 import Video from "../../components/video";
 import inspireTheNextGeneration from "./images/inspire-the-next-generation.png";
 import inspireTheNextGenerationSmall from "./images/inspire-the-next-generation-small.png";
-import howIBecameATeacher from "./images/how-i-became-a-teacher.png";
 
 export default () => (
   <div className={styles.container}>
     <div id={styles.inspireImageLargeContainer}>
+      {/* TODO: Replace image with text once we have got stock image background? */}
       <Link id={styles.anchorLifeAsATeacher} to="/life-as-a-teacher/" />
       <img
         src={inspireTheNextGeneration}
@@ -26,15 +26,36 @@ export default () => (
     </div>
     <div id={styles.howIBecameATeacherContainer}>
       <Video
+        id={styles.howIBecameATeacherVideo}
         videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
         videoTitle="Official Music Video on YouTube"
       />
-      <img
-        src={howIBecameATeacher}
-        alt="How I became a Teacher"
-        id={styles.howIBecameATeacherHeader}
-      />
-      <h3 id={styles.howIBecameATeacherText}>
+      <div id={styles.containerHeaderHowIBecameATeacher}>
+        <Link
+          id={styles.anchorHeaderHowIBecameATeacher}
+          to="/life-as-a-teacher/stories/"
+        >
+          <h1 id={styles.headerHowIBecameATeacher}>
+            <span id={styles.headerLineOneBackgroundHowIBecameATeacher}>
+              <div
+                className={styles.headerTextHowIBecameATeacher}
+                id={styles.headerLineOneHowIBecameATeacher}
+              >
+                How I became
+              </div>
+            </span>
+            <div>
+              <span
+                className={styles.headerTextHowIBecameATeacher}
+                id={styles.headerLineTwoHowIBecameATeacher}
+              >
+                a teacher
+              </span>
+            </div>
+          </h1>
+        </Link>
+      </div>
+      <h3 className={styles.howIBecameATeacherText}>
         Theo swapped his office job for a career as an English teacher. Read
         Theo's story
       </h3>
@@ -42,7 +63,9 @@ export default () => (
         to="/life-as-a-teacher/stories/"
         id={styles.howIBecameATeacherAnchor}
       >
-        <h3>View more stories&nbsp;&nbsp;&nbsp;&nbsp;></h3>
+        <h3 className={styles.howIBecameATeacherText}>
+          View more stories&nbsp;&nbsp;&nbsp;&nbsp;>
+        </h3>
       </Link>
     </div>
   </div>
