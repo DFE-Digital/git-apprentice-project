969969838a8c84e2f5219fb1a12d3fc1b099445c
FKANE <fg.kane@gmail.com>
2020-08-25 10:07:14 +0100

    Added accessiblity to config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+    variants: {
+        backgroundColor: ['active'],
+        accessibility: ['responsive', 'focus', 'hover', 'active']
+
+    },
+  plugins: [],
+}
