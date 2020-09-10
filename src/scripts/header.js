9d78a97b3ae2e3dd850ecb4fc196fcde3ad8479e
Nicholas Burness <nickburness93@outlook.com>
2020-09-10 13:44:32 +0100

    clean file structure, consolidate stylesheet and point pages to this change, formatted code on several pages

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,8 +1,10 @@
+let button = document.getElementById("nav-menu");
+let toggleIcon = document.getElementById("toggle-nav");
+let desktopView = window.matchMedia("(min-width: 768px)");
+
 function displayNavLinksOnMobileView() {
-  let button = document.getElementById("nav-menu");
   button.classList.toggle("hidden");
 
-  let toggleIcon = document.getElementById("toggle-nav");
   toggleIcon.classList.toggle("fa-window-close");
   toggleIcon.classList.toggle("fa-bars");
 }
@@ -10,11 +12,14 @@ function displayNavLinksOnMobileView() {
 function displayNavMenu(desktopView) {
   if (desktopView.matches) {
     // If media query matches
-    let button = document.getElementById("nav-menu");
     button.classList.remove("hidden");
+
+    toggleIcon.classList.toggle("fa-window-close");
+    toggleIcon.classList.toggle("fa-bars");
   }
 }
 
-let desktopView = window.matchMedia("(min-width: 768px)");
+
+
 displayNavMenu(desktopView);
 desktopView.addListener(displayNavMenu);
